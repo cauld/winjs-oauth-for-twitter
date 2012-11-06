5795b2474ec8a24bf72a63de1f75e017f1424200
Chad Auld <chad@wearemanifold.com>
2012-11-05 17:31:27 -0700

    Adding empty config file for sample

---
+++ js/config.js
@@ -0,0 +1,12 @@
+﻿/**
+ * WinJS OAuth for Twitter
+ * https://github.com/cauld/twitter-oauth-for-winjs
+ * Apache License, Version 2.0
+ */
+
+WinJS.Namespace.define("Twitter.OAuth.Config", {
+    consumerKey: '',
+    consumerSecret: '',
+    userOAuthToken: '',
+    userOAuthTokenSecret: ''
+});
